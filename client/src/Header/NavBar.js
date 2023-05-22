dec127755d390f5f9b3742cc8e7a45248a98e87d
Miladepour <epour.milad@gmail.com>
2023-05-22 15:41:15 +0100

    github pages confiq

--- client/src/Header/NavBar.js
+++ client/src/Header/NavBar.js
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import styles from './NavBar.module.css';
-
+import { SocialIcon } from 'react-social-icons';
 const NavBar = () => {
   return (
     <Navbar bg="black" expand="lg">
@@ -26,6 +26,15 @@ const NavBar = () => {
             >
               Download Resume
             </Nav.Link>
+            <SocialIcon
+              url="https://github.com/Miladepour"
+              className="socialmedia"
+              bgColor="#009290"
+            />
+            <SocialIcon
+              url="https://www.linkedin.com/in/milad-e-pour-a7a8a5239/"
+              className="socialmedia"
+            />
           </Nav>
         </Navbar.Collapse>
       </Container>
--- client/src/Hero/AboutMe.js
+++ client/src/Hero/AboutMe.js
@@ -1,50 +1,54 @@
-import { SocialIcon } from "react-social-icons";
+import { SocialIcon } from 'react-social-icons';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 const AboutMe = () => {
   return (
-    <div className="sectionAboutMe">
-      <section className="aboutMe">
-        <h3 className="aboutMeTitle">
-          <span className="aboutMeTitle">01.</span>About Me
-        </h3>
-        <hr className="dashed"></hr>
-        <p className="aboutMeText">
-          Hello! My name is Milad E-pour and I enjoy creating things that live
-          on the internet. My interest in web development started back in 2015
-          when I decided to try editing websites using WordPress and Wix.
-        </p>
-        <p className="aboutMeText">
-          My main focus these days is building accessible, inclusive products
-          and digital experiences at Upstatement for a variety of clients.
-        </p>
-        <SocialIcon
-          url="https://github.com/Miladepour"
-          className="socialmedia"
-          bgColor="#009290"
-        />
-        <SocialIcon
-          url="https://www.linkedin.com/in/milad-e-pour-a7a8a5239/"
-          className="socialmedia"
-        />
-      </section>
-      <section className="leftSideSection">
-        <p className="aboutMeText">
-          Here are a few technologies I’ve been working with recently:
-        </p>
-        <ul className="aboutMeList">
-          <li>JavaScript (ES6+)</li>
-          <li>React</li>
-          <li>Node.js</li>
-          <li>WordPress</li>
-          <li>MongoDB</li>
-          <li>SQL</li>
-        </ul>
-        <img
-          src="https://cynoco.co.uk/wp-content/uploads/2022/06/Untitled-design2.png"
-          alt="Milad Ebrahimpour"
-          className="personlImage"
-        />
-      </section>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          {' '}
+          <h3 className="aboutMeTitle">
+            <span className="aboutMeTitle">01.</span>About Me
+          </h3>
+          <p className="aboutMeText">
+            Hello! My name is Milad E-pour and I enjoy creating things that live
+            on the internet. My interest in web development started back in 2015
+            when I decided to try editing websites using WordPress and Wix.
+          </p>
+          <SocialIcon
+            url="https://github.com/Miladepour"
+            className="socialmedia"
+            bgColor="#009290"
+          />
+          <SocialIcon
+            url="https://www.linkedin.com/in/milad-e-pour-a7a8a5239/"
+            className="socialmedia"
+          />
+        </Col>
+        <Col>
+          {' '}
+          <section className="leftSideSection">
+            <p className="aboutMeText">
+              Here are a few technologies I’ve been working with recently:
+            </p>
+            <ul className="aboutMeList">
+              <li>JavaScript (ES6+)</li>
+              <li>React</li>
+              <li>Node.js</li>
+              <li>WordPress</li>
+              <li>MongoDB</li>
+              <li>SQL</li>
+            </ul>
+            <img
+              src="https://cynoco.co.uk/wp-content/uploads/2022/06/Untitled-design2.png"
+              alt="Milad Ebrahimpour"
+              className="personlImage"
+            />
+          </section>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
