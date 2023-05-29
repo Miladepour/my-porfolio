d47ddfc81f9a9529d6fc4d2c62eea4b10980c74d
Miladepour <epour.milad@gmail.com>
2023-05-29 13:39:50 +0100

    update css

--- client/src/Hero/Intro.js
+++ client/src/Hero/Intro.js
@@ -1,23 +1,25 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import styels from "./Intro.module.css";
+import styles from "./Intro.module.css";
 const Intro = () => {
   return (
-    <Container>
+    <Container className={styles.hero}>
       <Row>
         <Col>
-          <p className="heroFristP">Hi, my name is 👋🏼</p>
+          <p className={styles.heroFristP}>Hi, my name is 👋🏼</p>
         </Col>
+      </Row>
+      <Row>
         <Col>
-          <div className="nameAnimation">
-            <h1 className="typewriter">{"{Milad E-Pour}"}</h1>
-            <h2 className="textBelowName">I build things for the web.</h2>
-          </div>
+          <h1 className={styles.typewriter}>{"{Milad E-Pour};"}</h1>
+          <h2 className={styles.textBelowName}>I build things for the web.</h2>
         </Col>
+      </Row>
+      <Row>
         <Col>
-          <p className="heroSecondP">
-            I’m a Jr <span>software developer</span> specializing in building
+          <p className={styles.heroSecondP}>
+            I’m a Jr <span>Software Developer</span> specializing in building
             (and occasionally designing) exceptional digital experiences.
             Currently, I’m focused on improving my skill with backend and
             database.
@@ -25,12 +27,6 @@ const Intro = () => {
         </Col>
       </Row>
     </Container>
-    // <div>
-    //   <div className="heroSection">
-    //
-
-    //   </div>
-    // </div>
   );
 };
 
