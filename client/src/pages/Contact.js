56d8ea176d4ba9802ebba56fda26835f22eaa8ee
Miladepour <epour.milad@gmail.com>
2023-09-13 17:19:49 +0100

    update website

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,6 +1,6 @@
 import styles from "./Contact.module.css";
-import ContactFrom from "../Form/ContactForm";
-import { Container, Row, Col, Card, Button, ListGroup } from "react-bootstrap";
+// import ContactFrom from "../Form/ContactForm";
+import { Container, Row, Col,Image } from "react-bootstrap";
 
 const Contact = () => {
   return (
@@ -23,12 +23,20 @@ const Contact = () => {
           </p>
         </Col>
       </Row>
-      <Row className="pt-4 justify-content-around">
+      <Row className="pt-4 justify-content-around align-items-center">
         <Col>
-          <h2>Feel free to contact me by filling out the following form</h2>
+          <h2 style={{fontStyle:"bold", fontSize:"50px"}}>Milad Ebrahimpour</h2>
+          <h2>Get in touch</h2>
+          <h3>
+            I will get back to you as soon as possible. I look forward to
+            hearing from you!
+          </h3>
+          <h5>
+          epour.milad@gmail.com
+          </h5>
         </Col>
         <Col>
-          <ContactFrom />
+        <Image className={styles.contactImage} src="https://cynoco.co.uk/wp-content/uploads/2023/09/Untitled-design-7.png" fluid></Image>
         </Col>
       </Row>
     </Container>
